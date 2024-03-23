6574e7702775e7edaa5146d5534831c87f92a52e
murathany7 <murathany7@gmail.com>
2024-03-24 00:30:15 +0300

    Layer section and NavBar added

--- Frontend/ews-madagascar/src/App.js
+++ Frontend/ews-madagascar/src/App.js
@@ -1,8 +1,10 @@
 import Map from "./Components/Map";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Map />
     </div>
   );
--- Frontend/ews-madagascar/src/Components/Map.js
+++ Frontend/ews-madagascar/src/Components/Map.js
@@ -1,25 +1,91 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  LayerGroup,
+  WMSTileLayer,
+} from "react-leaflet";
+import { Switch, FormControlLabel, FormGroup } from "@mui/material";
+import Box from "@mui/material/Box";
 
-function Map() {
+const layerInfo = [
+  {
+    name: "Layer 1",
+    url: "geoserver_wms_url",
+    layer: "geoserver_layer_name",
+  },
+  {
+    name: "Layer 2",
+    url: "geoserver_wms_url",
+    layer: "geoserver_layer_name",
+  },
+  {
+    name: "Layer 3",
+    url: "geoserver_wms_url",
+    layer: "geoserver_layer_name",
+  },
+  {
+    name: "Layer 4",
+    url: "geoserver_wms_url",
+    layer: "geoserver_layer_name",
+  },
+  {
+    name: "Layer 5",
+    url: "geoserver_wms_url",
+    layer: "geoserver_layer_name",
+  },
+];
+
+export default function Map() {
+  const [activeLayers, setActiveLayers] = useState([]);
+
+  const toggleLayer = (layerName) => {
+    if (activeLayers.includes(layerName)) {
+      setActiveLayers(activeLayers.filter((name) => name !== layerName));
+    } else {
+      setActiveLayers([...activeLayers, layerName]);
+    }
+  };
+
   return (
-    <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
-      scrollWheelZoom={true}
-      style={{ width: "100%", height: "calc(100vh - 4rem)" }}
-    >
-      <TileLayer
-        url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <Box sx={{ display: "flex", height: "100vh" }}>
+      <Box sx={{ flex: 1, padding: "20px" }}>
+        <FormGroup>
+          {layerInfo.map((layer) => (
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={activeLayers.includes(layer.name)}
+                  onChange={() => toggleLayer(layer.name)}
+                />
+              }
+              label={layer.name}
+              key={layer.name}
+            />
+          ))}
+        </FormGroup>
+      </Box>
+      <Box sx={{ flex: 6, height: "100%" }}>
+        <MapContainer
+          center={[-19.373, 46.704]}
+          zoom={6}
+          style={{ width: "100%", height: "100%" }}
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          {layerInfo.map((layer) =>
+            activeLayers.includes(layer.name) ? (
+              <LayerGroup key={layer.name}>
+                <WMSTileLayer
+                  url={layer.url}
+                  layers={layer.layer}
+                  format="image/png"
+                  transparent={true}
+                />
+              </LayerGroup>
+            ) : null
+          )}
+        </MapContainer>
+      </Box>
+    </Box>
   );
 }
-
-export default Map;
