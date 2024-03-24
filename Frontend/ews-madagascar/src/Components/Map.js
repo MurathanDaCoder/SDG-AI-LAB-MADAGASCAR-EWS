0d87ca835c15b3e924da402695fb4dfe7149edbc
murathany7 <murathany7@gmail.com>
2024-03-24 21:47:32 +0300

    added layer from GeoServer and Legend to display on map

--- Frontend/ews-madagascar/src/Components/Map.js
+++ Frontend/ews-madagascar/src/Components/Map.js
@@ -4,15 +4,17 @@ import {
   TileLayer,
   LayerGroup,
   WMSTileLayer,
+  useMap,
 } from "react-leaflet";
+import L from "leaflet";
 import { Switch, FormControlLabel, FormGroup } from "@mui/material";
 import Box from "@mui/material/Box";
-
+import "./Map.css";
 const layerInfo = [
   {
-    name: "Layer 1",
-    url: "geoserver_wms_url",
-    layer: "geoserver_layer_name",
+    name: "NCLIMGRID Daily SPI 12mo",
+    url: "http://localhost:8080/geoserver/ne/wms",
+    layer: "ne:NCLIMGRID_DAILY-spi-12mo",
   },
   {
     name: "Layer 2",
@@ -84,8 +86,33 @@ export default function Map() {
               </LayerGroup>
             ) : null
           )}
+          <Legend />
         </MapContainer>
       </Box>
     </Box>
   );
+}
+
+function Legend() {
+  const map = useMap();
+
+  React.useEffect(() => {
+    const legend = L.control({ position: "bottomright" });
+
+    legend.onAdd = function () {
+      const div = L.DomUtil.create("div", "info legend");
+      div.innerHTML +=
+        '<img src="http://localhost:8080/geoserver/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=ne:NCLIMGRID_DAILY-spi-12mo&STYLE=testspi" alt="legend">';
+      return div;
+    };
+
+    legend.addTo(map);
+
+    // Cleanup function on unmount
+    return () => {
+      legend.remove();
+    };
+  }, [map]);
+
+  return null;
 }
